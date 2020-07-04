6e20cab3427d5b78d897e19bedc2973f7ea7f570
Dave <dave.gagnat@gmail.com>
2020-07-03 20:07:13 -0500

    Explored Activities, Bundles, Fields and Classes

--- 3-ButtonClickApp/app/src/main/java/com/example/a3_buttonclickapp/MainActivity.java
+++ 3-ButtonClickApp/app/src/main/java/com/example/a3_buttonclickapp/MainActivity.java
@@ -1,14 +1,24 @@
 package com.example.a3_buttonclickapp;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText userInput;
+    private Button button;
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        userInput = (EditText) findViewById(R.id.editTextTextPersonName);
+        button = (Button) findViewById(R.id.button);
+        textView = (TextView) findViewById(R.id.textView);
     }
 }
