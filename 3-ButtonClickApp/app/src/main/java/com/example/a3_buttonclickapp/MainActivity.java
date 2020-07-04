d034ad7e02fea369dfff3ad30e19d385b9ab40fd
Dave <dave.gagnat@gmail.com>
2020-07-04 15:49:52 -0500

    Explored activity lifecycles and overriding methods

--- 3-ButtonClickApp/app/src/main/java/com/example/a3_buttonclickapp/MainActivity.java
+++ 3-ButtonClickApp/app/src/main/java/com/example/a3_buttonclickapp/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.a3_buttonclickapp;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.method.ScrollingMovementMethod;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Button;
@@ -12,9 +13,11 @@ public class MainActivity extends AppCompatActivity {
 
     private EditText userInput;
     private TextView textView;
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.d(TAG, "onCreate: in");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
@@ -34,6 +37,21 @@ public class MainActivity extends AppCompatActivity {
             }
         };
         button.setOnClickListener(ourOnClickListener);
+        Log.d(TAG, "onCreate: out");
 
+    }
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
     }
 }
