43e28167c8bae70584bcbc4205c1ed4d7d01b882
Dave <dave.gagnat@gmail.com>
2020-07-04 16:30:59 -0500

    Explored how to use activity lifecyles to save information stored in bundle when screen rotates

--- 3-ButtonClickApp/app/src/main/java/com/example/a3_buttonclickapp/MainActivity.java
+++ 3-ButtonClickApp/app/src/main/java/com/example/a3_buttonclickapp/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.a3_buttonclickapp;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.method.ScrollingMovementMethod;
@@ -14,6 +16,7 @@ public class MainActivity extends AppCompatActivity {
     private EditText userInput;
     private TextView textView;
     private static final String TAG = "MainActivity";
+    private final String TEXT_CONTENTS = "TextContents";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,14 +47,54 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onStart() {
+        Log.d(TAG, "onStart: in");
         super.onStart();
+        Log.d(TAG, "onStart: out");
     }
+
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+        Log.d(TAG, "onRestoreInstanceState: in");
+        super.onRestoreInstanceState(savedInstanceState);
+//        String savedString = savedInstanceState.getString(TEXT_CONTENTS);
+//        textView.setText(savedString);
+        textView.setText(savedInstanceState.getString(TEXT_CONTENTS));
+        Log.d(TAG, "onRestoreInstanceState: out");
+    }
+
     @Override
     protected void onRestart() {
+        Log.d(TAG, "onRestart: in");
         super.onRestart();
+        Log.d(TAG, "onRestart: out");
     }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        Log.d(TAG, "onSaveInstanceState: in");
+        outState.putString(TEXT_CONTENTS, textView.getText().toString());
+        super.onSaveInstanceState(outState);
+        Log.d(TAG, "onSaveInstanceState: out");
+    }
+
+    @Override
+    protected void onPause() {
+        Log.d(TAG, "onPause: in");
+        super.onPause();
+        Log.d(TAG, "onPause: out");
+    }
+
+    @Override
+    protected void onStop() {
+        Log.d(TAG, "onStop: in");
+        super.onStop();
+        Log.d(TAG, "onStop: out");
+    }
+
     @Override
     protected void onDestroy() {
+        Log.d(TAG, "onDestroy: in");
         super.onDestroy();
+        Log.d(TAG, "onDestroy: out");
     }
 }
