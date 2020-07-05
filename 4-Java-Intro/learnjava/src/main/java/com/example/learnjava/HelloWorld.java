5e0c1e9f6327c12021a0e218e36208344205682a
Dave <dave.gagnat@gmail.com>
2020-07-04 19:07:22 -0500

    Setup Java Intro

---
+++ 4-Java-Intro/learnjava/src/main/java/com/example/learnjava/HelloWorld.java
@@ -0,0 +1,8 @@
+package com.example.learnjava;
+
+public class HelloWorld {
+    public static void main(String[] args) { // psvm tab
+        System.out.println("Hello World :);)"); // sout tab
+        System.out.println("Hello World!!!!! My first Java program");
+    }
+}
