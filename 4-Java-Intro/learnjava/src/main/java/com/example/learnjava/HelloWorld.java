3ece552d1c8474d40edb0cbd0bb7ad1d44ac24e3
Dave <dave.gagnat@gmail.com>
2020-07-05 21:15:34 -0500

    Explored feilds, variables, declaring, asigning

--- 4-Java-Intro/learnjava/src/main/java/com/example/learnjava/HelloWorld.java
+++ 4-Java-Intro/learnjava/src/main/java/com/example/learnjava/HelloWorld.java
@@ -4,5 +4,15 @@ public class HelloWorld {
     public static void main(String[] args) { // psvm tab
         System.out.println("Hello World :);)"); // sout tab
         System.out.println("Hello World!!!!! My first Java program");
+
+        String tim; // declared
+        tim = "Tim Buchalka"; // assigned
+//        tim = "12"; // Java does not allow type changes that differ from how it was declared
+        System.out.println(tim);
+
+        int timSalary = 32; // declared and assigned
+        int monthly = timSalary * 4;
+        System.out.println(timSalary);
+        System.out.println(monthly);
     }
 }
