c67fb49870f6c5f9211ee962ee5e1cb4cd0b5410
Dave <dave.gagnat@gmail.com>
2020-07-06 09:43:36 -0500

    Explored classes, overriding constructors, getters/setters

--- 4-Java-Intro/learnjava/src/main/java/com/example/learnjava/Demo.java
+++ 4-Java-Intro/learnjava/src/main/java/com/example/learnjava/Demo.java
@@ -6,5 +6,18 @@ public class Demo {
         System.out.println(tim.getHandleName()); // handleName is set to private (access modifier)
         tim.setHandleName("Tim");
         System.out.println(tim.getHandleName());
+        System.out.println("Level: " + tim.getLevel());
+        System.out.println("Lives: " + tim.getLives());
+
+        Player louise = new Player("Louise");
+        System.out.println(louise.getHandleName());
+        louise.setLives(5);
+        System.out.println("Level: " + louise.getLevel());
+        System.out.println("Lives: " + louise.getLives());
+
+        Player gr8 = new Player("gr8", 9);
+        System.out.println(gr8.getHandleName());
+        System.out.println("Level: " + gr8.getLevel());
+        System.out.println("Lives: " + gr8.getLives());
     }
 }
--- 4-Java-Intro/learnjava/src/main/java/com/example/learnjava/Player.java
+++ 4-Java-Intro/learnjava/src/main/java/com/example/learnjava/Player.java
@@ -8,10 +8,22 @@ public class Player {
     private int score;
 
     public Player() {
-        handleName = "Unknown player";
-        lives = 3;
-        level = 1;
-        score = 0;
+        this("Unknown player"); // default name
+    }
+
+    public Player(String name) { // Overloading the constructor
+        this(name, 1); // default level
+//        handleName = name;
+//        lives = 3;
+//        level = 1;
+//        score = 0;
+    }
+
+    public Player(String name, int startingLevel) { // Overloading the constructor
+        handleName = name;
+        lives = 3; // default lives
+        level = startingLevel;
+        score = 0; // default score
     }
 
 
@@ -24,5 +36,30 @@ public class Player {
             return;
         }
         handleName = name;
+    }
+
+    // Android Studio generates getters and setters for us // cmd + n
+    public int getLives() {
+        return lives;
+    }
+
+    public void setLives(int lives) {
+        this.lives = lives;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
     }
 }
