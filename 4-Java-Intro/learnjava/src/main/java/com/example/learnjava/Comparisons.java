41fb746b116d1005d4585c57041f0863194899eb
Dave <dave.gagnat@gmail.com>
2020-07-06 20:10:10 -0500

    Explored comparisons, .equals and ==

---
+++ 4-Java-Intro/learnjava/src/main/java/com/example/learnjava/Comparisons.java
@@ -0,0 +1,30 @@
+package com.example.learnjava;
+
+public class Comparisons {
+    public static void main(String[] args) { // psvm tab
+        int value = 4;
+        for (int i = 0; i < 8; i++) {
+            if (i == value) {
+                System.out.println(i + " is equal to " + value);
+            }
+            if (i <= value) {
+                System.out.println(i + " is less than or equal to " + value);
+            }
+            if (i >= value) {
+                System.out.println(i + " is greater than or equal to " + value);
+            }
+        }
+
+        System.out.println(4 != value);
+        System.out.println(5 != value);
+
+        System.out.println("\n========================");
+        String greeting = "Hello Tim";
+        String greeting2 = "Hello";
+        greeting2 += " Tim";
+        System.out.println(greeting);
+        System.out.println(greeting2);
+        System.out.println(greeting == greeting2); // do not use == when testing strings
+        System.out.println(greeting.equals(greeting2)); // str1.equals(str2) when testing strings
+    }
+}
--- 4-Java-Intro/learnjava/src/main/java/com/example/learnjava/Demo.java
+++ 4-Java-Intro/learnjava/src/main/java/com/example/learnjava/Demo.java
@@ -106,40 +106,40 @@ public class Demo { // cmd + 1 opens project // cmd + 4 opens run // opt + fn + 
 //            System.out.println("========================================");
 //        }
 
-        VampireKing boss = new VampireKing("Boss");
-        boss.showInfo();
-
-        boss.setLives(0);
-
-        do { // do while loop ensures that the code gets executed at least once
-            if(boss.dodges()) {
-                boss.setLives(boss.getLives() + 1);
-                continue;
-            }
-            if(boss.runAway()) {
-                System.out.println("Boss ran away");
-                break;
-            } else {
-                boss.takeDamage(80);
-                boss.showInfo();
-            }
-        } while (boss.getLives() > 0); // test is not done until the end
-        System.out.println("========================================");
-
-
-//        Player conan = new Player("Conan");
-//        conan.pickUpLoot(new Loot("Invisibility", LootType.POTION, 4));
-//        conan.pickUpLoot(new Loot("Mithril", LootType.ARMOR, 183));
-//        conan.pickUpLoot(new Loot("Ring of speed", LootType.RING, 25));
-//        conan.pickUpLoot(new Loot("Red Potion", LootType.POTION, 2));
-////        conan.pickUpLoot(new Loot("Cursed Shield", LootType.ARMOR, -8));
-//        conan.pickUpLoot(new Loot("Brass Ring", LootType.RING, 1));
-//        conan.pickUpLoot(new Loot("Chain Mail", LootType.ARMOR, 4));
-//        conan.pickUpLoot(new Loot("Gold Ring", LootType.RING, 12));
-//        conan.pickUpLoot(new Loot("Health Potion", LootType.POTION, 3));
-//        conan.pickUpLoot(new Loot("Silver Ring", LootType.RING, 6));
-//        conan.showInventory();
+//        VampireKing boss = new VampireKing("Boss");
+//        boss.showInfo();
 //
-//        System.out.println(conan.score());
+//        boss.setLives(0);
+//
+//        do { // do while loop ensures that the code gets executed at least once
+//            if(boss.dodges()) {
+//                boss.setLives(boss.getLives() + 1);
+//                continue;
+//            }
+//            if(boss.runAway()) {
+//                System.out.println("Boss ran away");
+//                break;
+//            } else {
+//                boss.takeDamage(80);
+//                boss.showInfo();
+//            }
+//        } while (boss.getLives() > 0); // test is not done until the end
+//        System.out.println("========================================");
+
+
+        Player conan = new Player("Conan");
+        conan.pickUpLoot(new Loot("Invisibility", LootType.POTION, 4));
+        conan.pickUpLoot(new Loot("Mithril", LootType.ARMOR, 183));
+        conan.pickUpLoot(new Loot("Ring of speed", LootType.RING, 25));
+        conan.pickUpLoot(new Loot("Red Potion", LootType.POTION, 2));
+        conan.pickUpLoot(new Loot("Cursed Shield", LootType.ARMOR, -8));
+        conan.pickUpLoot(new Loot("Brass Ring", LootType.RING, 1));
+        conan.pickUpLoot(new Loot("Chain Mail", LootType.ARMOR, 4));
+        conan.pickUpLoot(new Loot("Gold Ring", LootType.RING, 12));
+        conan.pickUpLoot(new Loot("Health Potion", LootType.POTION, 3));
+        conan.pickUpLoot(new Loot("Silver Ring", LootType.RING, 6));
+        conan.showInventory();
+
+        System.out.println(conan.score());
     }
 }
