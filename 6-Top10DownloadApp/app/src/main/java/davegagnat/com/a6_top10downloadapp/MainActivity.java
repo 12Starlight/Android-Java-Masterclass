c8577e3b2ad1a13cc67c93b4cdc081432bc61954
Dave <dave.gagnat@gmail.com>
2020-07-08 10:03:16 -0500

    Overrode onPostExecute and doInBackground

--- 6-Top10DownloadApp/app/src/main/java/davegagnat/com/a6_top10downloadapp/MainActivity.java
+++ 6-Top10DownloadApp/app/src/main/java/davegagnat/com/a6_top10downloadapp/MainActivity.java
@@ -4,8 +4,11 @@ package davegagnat.com.a6_top10downloadapp;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+    //logt tab
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,19 +18,29 @@ public class MainActivity extends AppCompatActivity {
 //        while(true) { // this stops the app from finishing and causes a crash
 //            int x = 1;
 //        }
+
+        Log.d(TAG, "onCreate: starting AsyncTask");
+        DownloadData downloadData = new DownloadData();
+        downloadData.execute("URL goes here");
+        Log.d(TAG, "onCreate: onCreate: done");
     }
 
     // Inner Class
     private class DownloadData extends AsyncTask<String, Void, String> {
+        private static final String TAG = "DownloadData";
+
+
         // ctr + O to get list of methods from this class that can be overrode
         @Override
         protected void onPostExecute(String s) {
             super.onPostExecute(s);
+            Log.d(TAG, "onPostExecute: parameter is " + s);
         }
 
         @Override
         protected String doInBackground(String... strings) {
-            return null;
+            Log.d(TAG, "doInBackground: starts with " + strings[0]);
+            return "doInBackground completed.";
         }
     }
 }
