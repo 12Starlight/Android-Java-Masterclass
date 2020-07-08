bd6a7fc72d2fca05fd69ea935392903548c37292
Dave <dave.gagnat@gmail.com>
2020-07-08 08:59:42 -0500

    Setup Top10DownloadableApp

---
+++ 6-Top10DownloadApp/app/src/main/java/davegagnat/com/a6_top10downloadapp/MainActivity.java
@@ -0,0 +1,14 @@
+package davegagnat.com.a6_top10downloadapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
