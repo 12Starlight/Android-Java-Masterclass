79561932418dafae64f90a0af5c3d9a4d36aaf3f
Dave <dave.gagnat@gmail.com>
2020-07-08 09:32:05 -0500

    Built inner class for DownloadData

--- 6-Top10DownloadApp/app/src/main/java/davegagnat/com/a6_top10downloadapp/MainActivity.java
+++ 6-Top10DownloadApp/app/src/main/java/davegagnat/com/a6_top10downloadapp/MainActivity.java
@@ -1,7 +1,8 @@
 package davegagnat.com.a6_top10downloadapp;
 
+// opt + return imports classes that are being used
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.os.AsyncTask;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +11,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//        while(true) { // this stops the app from finishing and causes a crash
+//            int x = 1;
+//        }
+    }
+
+    // Inner Class
+    private class DownloadData extends AsyncTask<String, Void, String> {
+        // ctr + O to get list of methods from this class that can be overrode
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+        }
+
+        @Override
+        protected String doInBackground(String... strings) {
+            return null;
+        }
     }
 }
