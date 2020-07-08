b2e10fec8598aaf899ab76446e739bcc0bf58e61
Dave <dave.gagnat@gmail.com>
2020-07-08 15:08:18 -0500

    Updated parse method to include try block

--- 6-Top10DownloadApp/app/src/main/java/davegagnat/com/a6_top10downloadapp/ParsApplications.java
+++ 6-Top10DownloadApp/app/src/main/java/davegagnat/com/a6_top10downloadapp/ParsApplications.java
@@ -1,5 +1,7 @@
 package davegagnat.com.a6_top10downloadapp;
 
+import android.util.Log;
+
 import org.xmlpull.v1.XmlPullParser;
 import org.xmlpull.v1.XmlPullParserFactory;
 
@@ -20,7 +22,7 @@ public class ParsApplications {
 
     public boolean parse(String xmlData) {
         boolean status = true;
-        FeedEntry currentRecord;
+        FeedEntry currentRecord = null;
         boolean inEntry = false;
         String textValue = "";
 
@@ -31,7 +33,42 @@ public class ParsApplications {
             xpp.setInput(new StringReader(xmlData));
             int eventType = xpp.getEventType();
             while(eventType != XmlPullParser.END_DOCUMENT) {
+                String tagName = xpp.getName();
+                switch (eventType) {
+                    case XmlPullParser.START_TAG:
+                        Log.d(TAG, "parse: Starting tag for " + tagName);
+                        if ("entry".equalsIgnoreCase(tagName)) {
+                            inEntry = true;
+                            currentRecord = new FeedEntry();
+                        }
+                        break;
+                    case XmlPullParser.TEXT:
+                        textValue = xpp.getText();
+                        break;
+                    case XmlPullParser.END_TAG:
+                        Log.d(TAG, "parse: Ending tag for " + tagName);
+                        if (inEntry) {
+                            if ("entry".equalsIgnoreCase(tagName)) {
+                                applications.add(currentRecord);
+                                inEntry = false;
+                            } else if ("name".equalsIgnoreCase(tagName)) {
+                                currentRecord.setName(textValue);
+                            } else if ("artist".equalsIgnoreCase(tagName)) {
+                                currentRecord.setArtist(textValue);
+                            } else if ("releaseDave".equalsIgnoreCase(tagName)) {
+                                currentRecord.setReleaseDate(textValue);
+                            } else if ("summary".equalsIgnoreCase(tagName)) {
+                                currentRecord.setSummary(textValue);
+                            } else if ("image".equalsIgnoreCase(textValue)) {
+                                currentRecord.setImageURL(textValue);
+                            }
+                        }
+                        break;
 
+                    default:
+                        // Nothing else to do
+                }
+                eventType = xpp.next();
             }
         } catch(Exception e) {
             status = false;
